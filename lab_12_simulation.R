3a2de8ca787dc56317b135d55fe6ca860dc1f8aa
Aditya Darshan Gandhi <adityagandhi@Adityas-MBP.wv.cc.cmu.edu>
2019-07-31 13:42:20 -0400

    Change mode to list returned, names added.

--- lab_12_simulation.R
+++ lab_12_simulation.R
@@ -5,5 +5,5 @@
 generate_data <- function(n, p) {
   covariates <- matrix(data = rnorm(n = n, mean = 0, sd = 1), nrow = n, ncol = p)
   responses <- as.vector(rnorm(n = n, mean = 0, sd = 1))
-  return(list(covariates, responses))
+  return(list(covariates = covariates, responses = responses))
 }
