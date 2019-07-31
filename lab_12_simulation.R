d7de67d39a7cd6a6114b3f5fcbd3013e9f00164c
Aditya Darshan Gandhi <adityagandhi@Adityas-MBP.wv.cc.cmu.edu>
2019-07-31 13:57:21 -0400

    Change 2 made to the covariates matrix.

--- lab_12_simulation.R
+++ lab_12_simulation.R
@@ -3,7 +3,7 @@
 # Q2a
 
 generate_data <- function(n, p) {
-  covariates <- matrix(data = rnorm(n = np, mean = 0, sd = 1), nrow = n, ncol = p)
+  covariates <- matrix(data = rnorm(n = (n * p), mean = 0, sd = 1), nrow = n, ncol = p)
   responses <- as.vector(rnorm(n = n, mean = 0, sd = 1))
   return(list(covariates = covariates, responses = responses))
 }
