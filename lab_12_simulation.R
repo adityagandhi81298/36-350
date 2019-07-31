57bf1056713f245912534f78931ae84acdafed6c
Aditya Darshan Gandhi <adityagandhi@Adityas-MBP.wv.cc.cmu.edu>
2019-07-31 13:25:49 -0400

    Comitting Q2a.

---
+++ lab_12_simulation.R
@@ -0,0 +1,9 @@
+# Q2
+
+# Q2a
+
+generate_data <- function(n, p) {
+  covariates <- matrix(data = rnorm(n = n, mean = 0, sd = 1), nrow = n, ncol = p)
+  responses <- as.vector(rnorm(n = n, mean = 0, sd = 1))
+  return(list(covariates, responses))
+}
